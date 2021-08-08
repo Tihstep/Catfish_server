f1b093db4e251d61698b97e1ba2bd78e5426e5f3
Stepan Tikhomirov <63906533+Tihstep@users.noreply.github.com>
2021-08-08 16:22:42 +0600

    Delete predict.py

--- predict.py
+++
@@ -1,8 +0,0 @@
-img_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Felis_silvestris_silvestris.jpg/208px-Felis_silvestris_silvestris.jpg"
-img = Image.open(urlopen(img_url))
-img_tensor = transforms(img)[np.newaxis,:]
-img_tensor = img_tensor.to('cuda')
-print(type(img_tensor))
-prediction = CatfishModel(img_tensor)
-predicted_class = CatfishClasses[torch.argmax(prediction)]
-print(prediction, predicted_class)
